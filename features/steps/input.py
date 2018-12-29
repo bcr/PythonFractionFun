ef82fb7aff1d214743bd06f6348c9562cfb97794
Blake Ramsdell <blake.ramsdell@onelogin.com>
2018-12-28 17:05:17 -0800

    Better assertion output.

--- features/steps/input.py
+++ features/steps/input.py
@@ -5,6 +5,9 @@ import fractionfun
 def step_impl(context, input):
     context.output = fractionfun.process_input(input)
 
+def assert_strings_equal(expected, actual):
+    assert expected == actual, "Expected {}, but was {}".format(expected, actual)
+
 @then(u'the output should be "{output}"')
 def step_impl(context, output):
-    assert output == context.output
+    assert_strings_equal(output, context.output)
